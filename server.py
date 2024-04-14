f0230018a5c32451f12e60e68712922de3041e04
TheSniterSun <amanjarrez36@outlook.com>
2024-04-14 19:45:00 -0400

    Card Types Learning Content Almost Done

--- server.py
+++ server.py
@@ -193,6 +193,50 @@ def learn(id):
     return render_template('kdrama.html', item=item, itemActor=itemActor, itemGenre=itemGenre)
 
 '''
+Access Types
+'''
+
+@app.route('/type/<int:id>', methods=['GET']) # GET request because just requesting info from server
+def type(id):
+    item = None
+    number = 0
+
+    print(data)
+
+    for drama in data:
+        if drama['id'] == int(id): # str by default
+            item = drama
+            break
+    
+    # matching item by actor
+    itemActor = None
+    itemGenre = None
+
+    if item: # item exists
+        actors = item['actors']
+        genres = item['genres']
+
+        for actor in actors: # for each actor in this particular kdrama
+            for drama in data: # check if existing match in data
+                if drama['id'] != int(id):
+                    for actor2 in drama['actors']: # (BUT CANNOT BE THE SAME ITEM)
+                        if actor.lower() == actor2.lower():
+                            itemActor = drama
+                            break
+
+        for genre in genres: # for each actor in this particular kdrama
+            for drama in data: # check if existing match in data
+                if drama['id'] != int(id) and genre in drama['genres']: # (BUT CANNOT BE THE SAME ITEM)
+                    itemGenre = drama
+                    break
+
+    # print(item)
+    # print(item)
+    number = id
+
+    return render_template('types.html', item=item, itemActor=itemActor, itemGenre=itemGenre, number=number)
+
+'''
 Access the add a new kdrama page OR add a new kdrama
 '''
 @app.route('/add', methods=['POST', 'GET']) # GET request because just requesting info from server
